cd0dcb56c783756810aa427429e8736e7ca8db28
Pablo Eguilaz Pérez <UO308329@uniovi.es>
2025-01-30 11:18:38 +0100

    Ejercicio 1.1.1

--- 00_Bicycle 1.0/src/BicycleDemo.java
+++ 00_Bicycle 1.0/src/BicycleDemo.java
@@ -10,6 +10,14 @@ class BicycleDemo {
           Bicycle bike1 = new Bicycle(50,10,2);
           Bicycle bike2 = new Bicycle();
           Bicycle bike3 = new Bicycle();
+          
+          /* 
+           * Añade operaciones al ejemplo de uso en el método main(). Por ejemplo, crea
+           * otra bicicleta con la misma gear que bike1 y con el doble de speed de
+           * bike2.
+           */
+          
+          Bicycle bike4 = new Bicycle(bike1.getCadence(), bike2.getSpeed() * 2, 1);
 
           // Invoke methods on those objects
 
