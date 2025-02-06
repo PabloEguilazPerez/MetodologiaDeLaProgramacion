391f300aec2fdf31da3beff8e5cc4fa2555e338d
Pablo Eguilaz Pérez <UO308329@uniovi.es>
2025-02-06 11:52:08 +0100

    Ejercicio 1.3.1.

--- Practica_1_3_Relaciones_de_composicion/src/BasePlusCommissionEmployee.java
+++ Practica_1_3_Relaciones_de_composicion/src/BasePlusCommissionEmployee.java
@@ -1,103 +1,113 @@
-// Fig. 9.4: CommissionEmployee.java
-// CommissionEmployee class represents an employee paid a 
-// percentage of gross sales.
-public class CommissionEmployee extends Object
-{
-   private String firstName;
-   private String lastName;
-   private String socialSecurityNumber;
-   private double grossSales; // gross weekly sales
-   private double commissionRate; // commission percentage
+// Fig. 9.6: BasePlusCommissionEmployee.java
+// BasePlusCommissionEmployee class represents an employee that receives
+// a base salary in addition to commission.
 
-   // five-argument constructor
-   public CommissionEmployee( String first, String last, String ssn, 
-      double sales, double rate )
+public class BasePlusCommissionEmployee
+{
+   private CommissionEmployee e;
+   private double baseSalary; // base salary per week
+
+   // six-argument constructor
+   public BasePlusCommissionEmployee( String first, String last, 
+      String ssn, double sales, double rate, double salary )
    {
       // implicit call to Object constructor occurs here
-      firstName = first;
-      lastName = last;
-      socialSecurityNumber = ssn;
+	  e = new CommissionEmployee(first, last, ssn, sales, rate);
       setGrossSales( sales ); // validate and store gross sales
       setCommissionRate( rate ); // validate and store commission rate
-   } // end five-argument CommissionEmployee constructor
+      setBaseSalary( salary ); // validate and store base salary
+   } // end six-argument BasePlusCommissionEmployee constructor
 
    // set first name
    public void setFirstName( String first )
    {
-      firstName = first; // should validate
+      e.setFirstName(first);
    } // end method setFirstName
 
    // return first name
    public String getFirstName()
    {
-      return firstName;
+      return e.getFirstName();
    } // end method getFirstName
 
    // set last name
    public void setLastName( String last )
    {
-      lastName = last; // should validate
+      e.setLastName(last);
    } // end method setLastName
 
    // return last name
    public String getLastName()
    {
-      return lastName;
+      return e.getLastName();
    } // end method getLastName
 
    // set social security number
    public void setSocialSecurityNumber( String ssn )
    {
-      socialSecurityNumber = ssn; // should validate
+      e.setSocialSecurityNumber(ssn);
    } // end method setSocialSecurityNumber
 
    // return social security number
    public String getSocialSecurityNumber()
    {
-      return socialSecurityNumber;
+      return e.getSocialSecurityNumber();
    } // end method getSocialSecurityNumber
 
    // set gross sales amount
    public void setGrossSales( double sales )
    {
-      grossSales = ( sales < 0.0 ) ? 0.0 : sales;
+	   e.setGrossSales(( sales < 0.0 ) ? 0.0 : sales);
    } // end method setGrossSales
 
    // return gross sales amount
    public double getGrossSales()
    {
-      return grossSales;
+      return e.getGrossSales();
    } // end method getGrossSales
 
    // set commission rate
    public void setCommissionRate( double rate )
    {
-      commissionRate = ( rate > 0.0 && rate < 1.0 ) ? rate : 0.0;
+      e.setCommissionRate(( rate > 0.0 && rate < 1.0 ) ? rate : 0.0);
    } // end method setCommissionRate
 
    // return commission rate
    public double getCommissionRate()
    {
-      return commissionRate;
+      return e.getCommissionRate();
    } // end method getCommissionRate
+
+   // set base salary
+   public void setBaseSalary( double salary )
+   {
+      baseSalary = ( salary < 0.0 ) ? 0.0 : salary;
+   } // end method setBaseSalary
+
+   // return base salary
+   public double getBaseSalary()
+   {
+      return baseSalary;
+   } // end method getBaseSalary
 
    // calculate earnings
    public double earnings()
    {
-      return commissionRate * grossSales;
+      return baseSalary + ( e.getCommissionRate() * e.getGrossSales() );
    } // end method earnings
 
-   // return String representation of CommissionEmployee object
+   // return String representation of BasePlusCommissionEmployee
    @Override // indicates that this method overrides a superclass method
    public String toString()
    {
-      return String.format( "%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f", 
-         "commission employee", firstName, lastName, 
-         "social security number", socialSecurityNumber, 
-         "gross sales", grossSales, 
-         "commission rate", commissionRate );
+      return String.format( 
+         "%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f\n%s: %.2f", 
+         "base-salaried commission employee", e.getFirstName(), e.getLastName(), 
+         "social security number", e.getSocialSecurityNumber(), 
+         "gross sales", e.getGrossSales(), "commission rate", e.getCommissionRate(), 
+         "base salary", baseSalary );
    } // end method toString
-} // end class CommissionEmployee
+} // end class BasePlusCommissionEmployee
 
 
 /**************************************************************************
--- Practica_1_3_Relaciones_de_composicion/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Practica_1_3_Relaciones_de_composicion {
-}
