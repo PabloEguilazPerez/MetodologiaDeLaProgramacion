2988e2fdab4f150341fcf66eaef3c218f7ebb951
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2025-02-13 12:41:40 +0100

    Update

--- Chess/src/chess/units/King.java
+++ Chess/src/chess/units/King.java
@@ -12,81 +12,22 @@ import chess.utils.Position;
  * @author Juan Luis Mateo
  *
  */
-public class King {
-	private Position pos;
+public class King extends ChessPiece {
 
 	/**
 	 * Constructor
 	 * 
 	 */
 	public King() {
-		this.pos = null;
+		super();
 	}
 
-	/**
-	 * Get the position in the board
-	 * 
-	 * @return the position in the board
-	 */
-	public Position getPosition() {
-		return pos;
-	}
-
-	/**
-	 * Set the position of the piece in the board
-	 * 
-	 * @param pos new position of the piece in the board
-	 */
-	public void setPosition(Position pos) {
-		this.pos = pos;
-	}
 
 	public char toChar() {
 		return 'K';
 	}
 
-	/**
-	 * Returns an array with the valid squares where a King can move directly from
-	 * its current position:(N, S, E, W, NW, NE, SW and SE)
-	 * 
-	 * @param currentBoard the board with the Pieces
-	 * @return the vector of positions (possibly free) where it can move
-	 */
-	public DynamicVectorPosition validPositions(Board currentBoard) {
-
-		DynamicVectorPosition positions = null;
-
-		positions = generatePossiblePositions(currentBoard);
-
-		filterValidPositions(currentBoard, positions);
-
-		return positions;
-	}
-
-	/**
-	 * Filter the valid positions on a board of a dynamic vector of positions
-	 * 
-	 * @param board     the board
-	 * @param positions the vector of positions
-	 */
-	private void filterValidPositions(Board board, DynamicVectorPosition positions) {
-		int x = 0;
-		while (x < positions.length()) {
-			if (!board.freeSquare(positions.get(x)))
-				positions.remove(x);
-			else
-				x++;
-		}
-	}
-
-	/**
-	 * Return the dynamic array of possible squares where a King can move directly
-	 * from its current position:(N, S, E, W, NW, NE, SW and SE)
-	 * 
-	 * @param currentBoard the board
-	 * @return a dynamic vector of positions
-	 */
-	private DynamicVectorPosition generatePossiblePositions(Board currentBoard) {
+	protected DynamicVectorPosition generatePossiblePositions(Board currentBoard) {
 		DynamicVectorPosition positions = new DynamicVectorPosition();
 
 		// N
