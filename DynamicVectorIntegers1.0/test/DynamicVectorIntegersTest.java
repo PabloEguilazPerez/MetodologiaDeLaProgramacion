a78450f1c78821b9096f0da3562e0cf047525240
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2025-03-07 17:27:37 +0100

    Update

--- DynamicVectorIntegers1.0/test/DynamicVectorIntegersTest.java
+++ DynamicVectorIntegers1.0/test/DynamicVectorIntegersTest.java
@@ -4,14 +4,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DynamicVectorIntegersTest {
+	
+	DynamicVectorIntegers vI1;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		
+		vI1 = new DynamicVectorIntegers(new int[] { 1, 2, 3, 4, 5 });
+		
 	}
 
 	@Test
 	void testOnlyEven() {
-		fail("Not yet implemented");
+		DynamicVectorIntegers vI2 = new DynamicVectorIntegers(new int[] { 2, 4 });
+		assertEquals(vI2, vI1.onlyEven());
 	}
 
 }
